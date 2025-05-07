55b97c23ad237800292c94ddefc799293f5fe491
Justin Hill <justin@jmhill.dev>
2025-05-07 10:07:43 -0400

    initial commit - dotnet console app

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, dotnet on linux with helix editor!");
+
+Console.WriteLine("File watcher working...");
+
+Console.WriteLine("derp");
